a430a61b1500c5697df9cce8a59f8adb4c717a4c
Durga-velu <durgadevi134babu@gmail.com>
2025-03-28 01:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
-  
+
+	let table=document.getElementById("sampleTable")
+       
+	let cell1=document.createElement("td")
+   cell1.innerText="New Cell1"
+	let cell2=document.createElement("td")
+	cell2.innerText="New Cell2";
+    let newRow=table.insertRow(0)
+	newRow.appendChild(cell1)
+    newRow.appendChild(cell2)
 }
